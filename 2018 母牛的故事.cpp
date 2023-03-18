8d912be2997af675ff9bfbfc56563eeb522ab42b
金其涵 <1667716062@qq.com>
2023-03-18 22:33:46 +0800

    init

---
+++ 2018 母牛的故事.cpp
@@ -0,0 +1,33 @@
+// Problem Description  有一头母牛，它每年年初生一头小母牛。每头小母牛从第四个年头开始，每年年初也生一头小母牛。请编程实现在第n年的时候，共有多少头母牛？ 
+
+// Input  输入数据由多个测试实例组成，每个测试实例占一行，包括一个整数n(0<n<55)，n的含义如题目中描述。
+// n=0表示输入数据的结束，不做处理。 
+
+
+// Output  对于每个测试实例，输出在第n年的时候母牛的数量。
+// 每个输出占一行。 
+
+
+// Sample Input  2
+// 4
+// 5
+// 0 
+
+
+// Sample Output  2
+// 4
+// 6 
+
+#include <iostream>
+
+using namespace std;
+
+int fn(int n) {
+  return 
+}
+
+int main(){
+
+
+  return 0;
+}
---
+++ js/汉诺塔.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} A
+ * @param {number[]} B
+ * @param {number[]} C
+ * @return {void} Do not return anything, modify C in-place instead.
+ */
+var hanota = function (A, B, C) {
+	const movea2c = (size, A, B, C) => {
+		if (size == 1) {
+			const u = A.pop()
+			C.push(u)
+			return
+		} else {
+			const u = A.pop()
+			movea2c(size - 1, A, B, C)
+			C.push(u)
+		}
+	}
+	const size = A.length
+	movea2c(size, A, B, C)
+}
+
+hanota(A, B, C)
